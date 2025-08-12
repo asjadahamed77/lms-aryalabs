0d270c283d95d54f7fabb23d8f2501a06f6a55b5
asjadahamed77 <ahamedasjad77@gmail.com>
2025-08-12 06:37:52 +0530

    created Models for Assignment, Quiz, and Resource

--- server/models/associations.js
+++ server/models/associations.js
@@ -1,8 +1,12 @@
-// associations.js
 import User from './userModel.js';
 import Course from './courseModel.js';
+import Batch from './batchModel.js';
+import Assignment from './assignmentModel.js';
+import Resource from './resourceModel.js';
+import Quiz from './quizModel.js';
 
 export const setupAssociations = () => {
+  // Existing associations
   Course.belongsTo(User, {
     foreignKey: 'lecturerId',
     as: 'lecturer'
@@ -11,5 +15,61 @@ export const setupAssociations = () => {
   User.hasMany(Course, {
     foreignKey: 'lecturerId',
     as: 'courses'
+  });
+
+  // New associations
+  Assignment.belongsTo(User, {
+    foreignKey: 'lecturerId',
+    as: 'lecturer'
+  });
+
+  Assignment.belongsTo(Course, {
+    foreignKey: 'courseId',
+    as: 'course'
+  });
+
+  Assignment.belongsTo(Batch, {
+    foreignKey: 'batchId',
+    as: 'batch'
+  });
+
+  Resource.belongsTo(User, {
+    foreignKey: 'lecturerId',
+    as: 'lecturer'
+  });
+
+  Resource.belongsTo(Course, {
+    foreignKey: 'courseId',
+    as: 'course'
+  });
+
+  
+
+  
+
+  Quiz.belongsTo(User, {
+    foreignKey: 'lecturerId',
+    as: 'lecturer'
+  });
+
+  Quiz.belongsTo(Course, {
+    foreignKey: 'courseId',
+    as: 'course'
+  });
+
+  Quiz.belongsTo(Batch, {
+    foreignKey: 'batchId',
+    as: 'batch'
+  });
+
+  // Batch associations
+  Batch.hasMany(Assignment, {
+    foreignKey: 'batchId',
+    as: 'assignments'
+  });
+
+  Batch.hasMany(Quiz, {
+    foreignKey: 'batchId',
+    as: 'quizzes'
   });
 };
